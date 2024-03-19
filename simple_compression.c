d24a8c8652d7fa8abf9dfc261e0d0d6195e65e5e
elozsofi <elo.zsofi@gmail.com>
2024-03-19 12:02:23 +0100

    added timestamp for ACL, main-> packet structure

--- simple_compression.c
+++ simple_compression.c
@@ -3,70 +3,73 @@
 #include <string.h>    // strlen, strcat, memset
 #include <zstd.h>      // presumes zstd library is installed
 #include "common.h"    // Helper functions, CHECK(), and CHECK_ZSTD()
+#include <time.h>
 
 typedef struct Packet{
     size_t before;
     size_t after;
-    const int ip;
-    const double compress_rate;
+    int ip;
+    double compress_rate;
 }Packet;
 
 Packet* getInfoFromKernel(void* something){
     // TODO
 }
 
-//ACL szabályhoz kell: ip + rate limit (-> unsigned long long csomag/s fogadható)
-//kell timeouthoz timestamp
-//64bites unix timestamp kell clock_gettime()-ból
-void ACL(void) {
-    // TODO
+// needed for ACL rule: ip address + rate limit (defined as num of packets / second)
+// num of packets as an unsigned long long
+// timestamp needed for timeout
+// clock_gettime() -> 64b unix timestamp
+void ACL(const int ip, unsigned long long rateLimit) {
+    struct timespec ts;
+    unsigned long long tsm64;
+
+    clock_gettime(CLOCK_REALTIME, &ts);
+    tsm64 = ts.tv_sec * 1000000000L + ts.tv_nsec;
+
+    printf("Timestamp : %llu\n", tsm64);
 }
 
-//boundary kerneltől 8 MB out buffer (mainben)
-//függvényben logolni ha túl nagy a bemeneti buffer (errsys log -> printfelni)
+// boundary is 8MB (defined in main)
+// errsys log if input data size is too big (using printf for now)
 static void compress_orDie(void* data, size_t dataSize, void** compressedData, size_t* compressedSize, const size_t maxBufferSize)
 {
     // check dataSize
-    if (dataSize > maxBufferSize) { printf("bemeneti meret tul nagy!\n"); return; }
+    if (dataSize > maxBufferSize) { printf("Input size too big!\n"); return; }
 
-    // compressBound-dal a buffer kiszámolása
+    // calculate buffer size for compressed data with compressBoud
     size_t const cBuffSize = ZSTD_compressBound(dataSize);
-    *compressedData = malloc(cBuffSize); // tömörített adatnak itt memóriafoglalás buffer mérete alapján
-    if (*compressedData == NULL) { printf("nem sikerult memoriat foglalni!\n"); return; }
+    *compressedData = malloc(cBuffSize); // memory for compressed data based on its buffer size
+    if (*compressedData == NULL) { printf("Memory error!\n"); return; }
 
-    // compressedSize-ot lementem, hogy később struktúrába mehessen
+    // compressedSize needed for Packet struct
     *compressedSize = ZSTD_compress(*compressedData, cBuffSize, data, dataSize, 1);
     CHECK_ZSTD(*compressedSize);
 
-    // még jobb memória kihasználtság kedvéért 
+    // realloc for better memory usage
     *compressedData = realloc(*compressedData, *compressedSize);
-    if (*compressedData == NULL) { printf("Nem sikerult a memoria allokacio!\n"); return;}
-}
-
-static char* createOutFilename_orDie(const char* filename)
-{
-    size_t const inL = strlen(filename);
-    size_t const outL = inL + 5;
-    void* const outSpace = malloc_orDie(outL);
-    memset(outSpace, 0, outL);
-    strcat(outSpace, filename);
-    strcat(outSpace, ".zst");
-    return (char*)outSpace;
+    if (*compressedData == NULL) { printf("Memory allocation error!\n"); return;}
 }
 
 int main(int argc, const char** argv)
 {
+    // maximum input buffer size is 8MB
     const size_t maxBufferSize = 8 * 1024 * 1024;
 
-    // próba
-    char* teszt = "blabla blaaaaa teszt Elek!!!!!!";
-    size_t tesztMeret = strlen(teszt) + 1;
-    void* tomoritett = NULL;
-    size_t tomoritettMeret = 0;
-    compress_orDie(teszt, tesztMeret, &tomoritett, &tomoritettMeret, maxBufferSize);
+    void* compr = NULL;
+    size_t comprSize = 0;
+    void* something;
+    Packet* p = getInfoFromKernel(something);
+    compress_orDie(something, sizeof(something), &compr, &comprSize, maxBufferSize);
 
-    printf("Elotte: %zu B, utana: %zu B\n", tesztMeret, tomoritettMeret);
-    free(tomoritett);
+    // structuring packet
+    p->before = sizeof(something);
+    p->after = comprSize;
+    p->compress_rate = comprSize / sizeof(something) * 100;
+    p->ip = -1; // TODO: need ip address
+
+    printf("Before: %zu B, after: %zu B\n", sizeof(void), comprSize);
+    free(compr);
 
     return 0;
 }
