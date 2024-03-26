a3efbda1b18d7c14df43800128870425fc25c44a
Élő Zsófia Anna <elo.zsofi@gmail.com>
2024-03-26 16:36:40 +0100

    getInfoFromKernel now reads pcap files

--- simple_compression.c
+++ simple_compression.c
@@ -1,9 +1,14 @@
+#include <time.h>      // timestamp
 #include <stdio.h>     // printf
 #include <stdlib.h>    // free
 #include <string.h>    // strlen, strcat, memset
 #include <zstd.h>      // presumes zstd library is installed
 #include "common.h"    // Helper functions, CHECK(), and CHECK_ZSTD()
-#include <time.h>
+#include <dirent.h>
+
+#define MAX_FILE_NAME 256
+static void compress_orDie(void* data, size_t dataSize, void** compressedData, size_t* compressedSize, const size_t maxBufferSize);
+size_t loadFile_orDie(const char* fileName, void* buffer, size_t bufferSize);
 
 typedef struct Packet{
     size_t before;
@@ -12,9 +17,86 @@ typedef struct Packet{
     double compress_rate;
 }Packet;
 
-Packet* getInfoFromKernel(void* something){
-    // TODO
+// get list of .pcap files in the curr directory
+// used in loadInfo to get pcap files faster, make loading simpler
+int getPcapFiles(char fileList[][MAX_FILE_NAME], int maxFiles) {
+    DIR *d;
+    struct dirent *dir;
+    d = opendir(".");
+    if (d == NULL) {
+        return -1;
+    }
+
+    int count = 0;
+    while ((dir = readdir(d)) != NULL && count < maxFiles) {
+        if (strstr(dir->d_name, ".pcap")) {
+            strncpy(fileList[count++], dir->d_name, MAX_FILE_NAME-1);
+        }
+    }
+
+    closedir(d);
+    return count;
 }
+
+Packet* getInfoFromKernel(const size_t maxBufferSize){
+    char fileList[50][MAX_FILE_NAME];
+    int files = getPcapFiles(fileList, 10);
+    void* fileBuffer = malloc(maxBufferSize);
+    Packet packetList[files];
+    int packetIter = 0;
+
+    // making buffer for file
+    if (fileBuffer == NULL) { printf("Failed to load buffer\n"); }
+
+    if (files == 0) { printf("No .pcap files in directory\n"); return; }
+
+    for(int i = 0; i < files; i++) {
+
+        Packet p;
+
+        char* name = fileList[i];
+        printf("Loading %s for compression\n", name);
+
+        size_t fileSize = loadFile_orDie(name, fileBuffer, maxBufferSize);
+
+        void* compressedData = NULL;
+        size_t compressedSize = 0;
+
+        // needed for compression runtime
+        // fyi runtime stands for the total runtime of the 5 compressions
+        struct timespec start, end;
+        unsigned long long tsm1, tsm2;
+        clock_gettime(CLOCK_MONOTONIC, &start);
+        tsm1 = start.tv_sec * 1000000000L + start.tv_nsec;
+
+        // compressing a file 5 times
+        for(int j = 0; j < 5; j++){
+            compress_orDie(fileBuffer, fileSize, &compressedData, &compressedSize, maxBufferSize);
+        }
+        
+        // calculating compression runtime
+        clock_gettime(CLOCK_MONOTONIC, &end);
+        tsm2 = end.tv_sec * 1000000000L + end.tv_nsec;
+        float deltaTime = (float)(tsm2-tsm1);
+        deltaTime *= 0.000001;
+
+        // structuring packet
+        p.before = fileSize;
+        p.after = compressedSize;
+        p.ip = -1; // TODO
+        p.compress_rate = (double)compressedSize/fileSize*100;
+        packetList[packetIter++] = p;
+
+        printf("Before: %zu B\tafter: %zu B\t runtime: %.2fms\n", fileSize*8, compressedSize*8, deltaTime);
+        
+        // free buffer for next file compression
+        free(compressedData);
+        compressedData = NULL;
+    }
+    free(fileBuffer);
+    return packetList;
+}
+
 
 // needed for ACL rule: ip address + rate limit (defined as num of packets / second)
 // num of packets as an unsigned long long
@@ -45,10 +127,6 @@ static void compress_orDie(void* data, size_t dataSize, void** compressedData, s
     // compressedSize needed for Packet struct
     *compressedSize = ZSTD_compress(*compressedData, cBuffSize, data, dataSize, 1);
     CHECK_ZSTD(*compressedSize);
-
-    // realloc for better memory usage
-    *compressedData = realloc(*compressedData, *compressedSize);
-    if (*compressedData == NULL) { printf("Memory allocation error!\n"); return;}
 }
 
 int main(int argc, const char** argv)
@@ -56,20 +134,7 @@ int main(int argc, const char** argv)
     // maximum input buffer size is 8MB
     const size_t maxBufferSize = 8 * 1024 * 1024;
 
-    void* compr = NULL;
-    size_t comprSize = 0;
-    void* something;
-    Packet* p = getInfoFromKernel(something);
-    compress_orDie(something, sizeof(something), &compr, &comprSize, maxBufferSize);
-
-    // structuring packet
-    p->before = sizeof(something);
-    p->after = comprSize;
-    p->compress_rate = comprSize / sizeof(something) * 100;
-    p->ip = -1; // TODO: need ip address
-
-    printf("Before: %zu B, after: %zu B\n", sizeof(void), comprSize);
-    free(compr);
-
+    Packet* p = getInfoFromKernel(maxBufferSize);
+    
     return 0;
 }
