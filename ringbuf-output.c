24a21d52001e1366beb5840592b87196aabec0c4
Élő Zsófia Anna <elo.zsofi@gmail.com>
2024-04-24 12:51:34 +0200

    Update ringbuf-output.c

--- ringbuf-output.c
+++ ringbuf-output.c
@@ -10,6 +10,34 @@
 #include <arpa/inet.h>
 #include "ringbuf-output.skel.h"
 
+// IPC shared memory
+#include <fcntl.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <semaphore.h>
+#define SHM_NAME "/packet_shm"
+#define SHM_SIZE 4*1024*1024 // shared buffer size is 4MB
+#define SEM_EMPTY_NAME "/sem_empty"
+#define SEM_FULL_NAME "/sem_full"
+
+int counter = 1;
+
+void *shared_memory;
+sem_t *empty, *full;
+
+void init_shared_memory(){
+	int fd = shm_open(SHM_NAME, O_CREAT | O_RDWR, 0666);
+	ftruncate(fd, SHM_SIZE);
+    shared_memory = mmap(0, SHM_SIZE, PROT_WRITE, MAP_SHARED, fd, 0);
+
+	//close(fd);
+
+	empty = sem_open(SEM_EMPTY_NAME, O_CREAT, 0666, 1);
+	full = sem_open(SEM_FULL_NAME, O_CREAT, 0666, 0);
+}
+
 struct packet_info {
     __u32 src_ip;
     __u32 payload_size;
@@ -45,25 +73,30 @@ static void sig_handler(int sig)
 
 int handle_event(void *ctx, void *data, size_t data_sz)
 {
-	char src_ip_str[INET_ADDRSTRLEN];
-	struct packet_info *pkt = data;
-
-	if (data_sz != sizeof(*pkt)) {
-		fprintf(stderr, "Size mismatch %zu\n", data_sz);
-		return -1; 
+	// fill shared buffer
+	// shared buffert dinamikusan csökkenteni, tölteni -> globális változóval
+	// wait helyett check shared_memory méret
+	sem_wait(empty);
+	memcpy(shared_memory, data, data_sz < SHM_SIZE ? data_sz : SHM_SIZE);
+	sem_post(full);
+	
+	unsigned char* temp = data;
+	size_t i = 0;
+	printf("%d ", counter++);
+	for(; i!=data_sz; ++temp){
+  	  printf("%02x", *temp);
+	  i++;
 	}
-
-	// convert src ip
-	inet_ntop(AF_INET, &pkt->src_ip, src_ip_str, sizeof(src_ip_str));
-    printf("%s\t%uB\n", src_ip_str, pkt->payload_size);
-
+	printf("\n\n");
 	return 0;
 }
 
 int main(int argc, char **argv)
 {
+
+	init_shared_memory();
+
 	struct ring_buffer *rb = NULL;
-	struct ringbuf_output_bpf *skel;
 	int err;
 
 	/* Set up libbpf logging callback */
@@ -72,33 +105,21 @@ int main(int argc, char **argv)
 	/* Bump RLIMIT_MEMLOCK to create BPF maps */
 	bump_memlock_rlimit();
 
-	/* Load and verify BPF application */
-	skel = ringbuf_output_bpf__open_and_load();
-	if(!skel){
-		fprintf(stderr, "Failed to open and load BPF skeleton\n");
-		return 1;
-	}
-
 	/* Clean handling of Ctrl-C */
 	signal(SIGINT, sig_handler);
 	signal(SIGTERM, sig_handler);
 
-	//////////////////
-	err = ringbuf_output_bpf__attach(skel);
-	if (err) {
-		fprintf(stderr, "Failed to attach BPF skeleton\n");
-		goto cleanup;
+	/* File descriptor for rb map*/
+	int fd;
+	fd = bpf_obj_get("/sys/fs/bpf/tc/globals/rb");
+	if (fd < 0)
+	{
+		return -1;
 	}
 
-	rb = ring_buffer__new(bpf_map__fd(skel->maps.rb), handle_event, NULL, NULL);
-	if (!rb) {
-		err = -1;
-		fprintf(stderr, "Failed to create ring buffer\n");
-		goto cleanup;
-	}
-	/////////////////
+	rb = ring_buffer__new(fd,handle_event,NULL,NULL);
 
-	printf("%s\t%s\n", "SOURCE", "PAYLOAD");
+	printf("%s\n", "PACKET PAYLOAD");
 	while(!exiting){
 		err = ring_buffer__poll(rb, 100);
 		/* Ctrl-C will cause -EINTR */
@@ -114,7 +135,17 @@ int main(int argc, char **argv)
 
 cleanup:
 	ring_buffer__free(rb);
-	ringbuf_output_bpf__destroy(skel);
-
 	return err < 0 ? -err : 0;
+}
+
+
+int open_global()
+{
+	int fd;
+	fd = bpf_obj_get("/sys/fs/bpf/tc/globals/rb");
+	if (fd < 0)
+	{
+		return -1;
+	}
+	return fd;
 }
