50050f9c4362930797006a7b9736cdc41c8b85ac
Élő Zsófia Anna <elo.zsofi@gmail.com>
2024-05-07 15:34:14 +0200

    need debug

--- ringbuf-output.c
+++ ringbuf-output.c
@@ -8,7 +8,7 @@
 #include <bpf/libbpf.h>
 #include "common.h"
 #include <arpa/inet.h>
-#include "ringbuf-output.skel.h"
+//#include "ringbuf-output.skel.h"
 
 // IPC shared memory
 #include <fcntl.h>
@@ -22,9 +22,17 @@
 #define SEM_EMPTY_NAME "/sem_empty"
 #define SEM_FULL_NAME "/sem_full"
 
+/* packet data*/
+struct packet {
+	int ip;
+	//char payload[65*1024]; /*maximum TCP packet size is 65535kB*/
+	int prot;
+};
+
 int counter = 1;
 
 void *shared_memory;
+void *free_space;
 sem_t *empty, *full;
 
 void init_shared_memory(){
@@ -32,16 +40,16 @@ void init_shared_memory(){
 	ftruncate(fd, SHM_SIZE);
     shared_memory = mmap(0, SHM_SIZE, PROT_WRITE, MAP_SHARED, fd, 0);
 
+	free_space = shared_memory;
 	//close(fd);
 
 	empty = sem_open(SEM_EMPTY_NAME, O_CREAT, 0666, 1);
 	full = sem_open(SEM_FULL_NAME, O_CREAT, 0666, 0);
-}
 
-struct packet_info {
-    __u32 src_ip;
-    __u32 payload_size;
-};
+	sem_init(empty,1,0);
+	sem_init(full,1,0);
+		
+}
 
 int libbpf_print_fn(enum libbpf_print_level level, const char *format, va_list args)
 {
@@ -73,21 +81,40 @@ static void sig_handler(int sig)
 
 int handle_event(void *ctx, void *data, size_t data_sz)
 {
-	// fill shared buffer
-	// shared buffert dinamikusan csökkenteni, tölteni -> globális változóval
-	// wait helyett check shared_memory méret
-	sem_wait(empty);
-	memcpy(shared_memory, data, data_sz < SHM_SIZE ? data_sz : SHM_SIZE);
-	sem_post(full);
+	int value;
+	sem_getvalue(full, &value);
+	if(sem_trywait(empty) == 0){
+		if((shared_memory+SHM_SIZE) - free_space >= data_sz){
+			memcpy(shared_memory, data, data_sz);
+			free_space += data_sz;
+			sem_post(empty);
+		}
+		else{
+			sem_post(full);
+			printf("Buffer is done filling\n");
+		}
+	}
+	else if(value == 0){
+		printf("Buffer is emptied\n");
+		free_space = shared_memory;
+		sem_post(empty);
+	}
+	else{
+		printf("Buffer is full\n");
+	}
+
+	const struct packet *e = data;
+
+	printf("%d\n%d\n\n", e->ip, e->prot);
 	
-	unsigned char* temp = data;
+	/*unsigned char* temp = data;
 	size_t i = 0;
 	printf("%d ", counter++);
 	for(; i!=data_sz; ++temp){
   	  printf("%02x", *temp);
 	  i++;
 	}
-	printf("\n\n");
+	printf("\n\n");*/
 	return 0;
 }
 
@@ -119,7 +146,7 @@ int main(int argc, char **argv)
 
 	rb = ring_buffer__new(fd,handle_event,NULL,NULL);
 
-	printf("%s\n", "PACKET PAYLOAD");
+	printf("%s\n", "PACKET");
 	while(!exiting){
 		err = ring_buffer__poll(rb, 100);
 		/* Ctrl-C will cause -EINTR */
@@ -136,16 +163,4 @@ int main(int argc, char **argv)
 cleanup:
 	ring_buffer__free(rb);
 	return err < 0 ? -err : 0;
-}
-
-
-int open_global()
-{
-	int fd;
-	fd = bpf_obj_get("/sys/fs/bpf/tc/globals/rb");
-	if (fd < 0)
-	{
-		return -1;
-	}
-	return fd;
 }
